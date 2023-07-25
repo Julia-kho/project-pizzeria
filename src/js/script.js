d8fcea09a8c46ef32696e926d013bfc5e099b8c1
Julia Khonina <julia.khonina@gmail.com>
2023-07-25 22:30:02 +0200

    Reder products

--- src/js/script.js
+++ src/js/script.js
@@ -53,10 +53,31 @@ const select = {
   };
 
 class Product{
-  constructor(){
+  constructor(id, data){
     const thisProduct = this;
 
+    thisProduct.id = id;
+    thisProduct.data = data;
+
+    thisProduct.renderInMenu();
+
     console.log('new Product', thisProduct);
+  }
+
+  renderInMenu(){
+    const thisProduct = this;
+
+    /*Generate HTML based on template */
+    const generatedHTML = templates.menuProduct(thisProduct.data);
+
+    /*Create element using utils.CreateElementFromHTML */
+    thisProduct.element = utils.createDOMFromHTML(generatedHTML);
+
+    /* find menu container */
+    const menuContainer = document.querySelector(select.containerOf.menu);
+
+    /*add element to menu*/
+    menuContainer.appendChild(thisProduct.element);
   }
 }
 
